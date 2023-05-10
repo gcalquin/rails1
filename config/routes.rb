7aba2006f8c4ff24de5794dc3009ca6ef2b0ac83
gcalquin <gcalquin@gmail.com>
2023-05-10 17:11:54 -0400

    se agrega controlador about y se agrega parte ejercicio deposrtes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'deportes/index'
+  get 'pages/index'
+  get 'pages/about'
   get 'welcome/index'
   get 'technology/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root "welcome#index"
 end
